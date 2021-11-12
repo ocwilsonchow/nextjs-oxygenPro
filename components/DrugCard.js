e2960d0fc301585cd48d27deb4c6cce04c7cdc45
John Doe <84591288+ocwilsonchow@users.noreply.github.com>
2021-11-13 00:47:15 +0800

    update

--- components/DrugCard.js
+++ components/DrugCard.js
@@ -24,9 +24,20 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbSeparator,
-  useColorModeValue
+  useColorModeValue,
+  Popover,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
 } from "@chakra-ui/react";
-import { ChevronRightIcon } from "@chakra-ui/icons";
+import { ChevronRightIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { AiFillWarning, AiFillCheckCircle } from "react-icons/ai";
 
 function DrugCard({ posts, width }) {
@@ -38,11 +49,12 @@ function DrugCard({ posts, width }) {
 
   return (
     <Flex flexWrap="wrap" p={5}>
-      <Flex my={3} mx={5}>
+      <Flex my={3} mx={5} width="100%" alignItems="center" justifyContent="space-between">
         <Breadcrumb
           spacing="8px"
           separator={<ChevronRightIcon color="gray.500" />}
           fontWeight="bold"
+          
         >
           <BreadcrumbItem>
             <BreadcrumbLink href="#">Home</BreadcrumbLink>
@@ -58,176 +70,191 @@ function DrugCard({ posts, width }) {
             </BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
+        <Flex>
+          <Menu>
+            <MenuButton as={Button} size="sm" bg={cardBackground} rightIcon={<ChevronDownIcon />}>
+              More
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Clinical Guidelines</MenuItem>
+              <MenuItem>Interactions</MenuItem>
+              <MenuItem>Treatment Summaries</MenuItem>
+              <MenuItem>Add to my list</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
       </Flex>
       <Divider orientation="horizontal" mb={5} />
       <Flex flexWrap="wrap" justifyContent="center">
-      {posts.map((post, index) => {
-        return (
-          <GridItem
-            key={index}
-            m={3}
-            p={5}
-            bg={cardBackground}
-            maxH={1000}
-            width={width>=1000? "31%" : "100%"}
-            borderRadius="lg"
-            overflowY="auto"
-            css={{
-              "&::-webkit-scrollbar": {
-                width: "4px",
-              },
-              "&::-webkit-scrollbar-track": {
-                width: "3px",
-              },
-              "&::-webkit-scrollbar-thumb": {
-                background: "#1D4044",
-                borderRadius: "24px",
-              },
-            }}
-          >
-            <Text fontWeight="bold" fontSize="3xl" mb={3}>
-              {post.drugName.map((name, i) => {
-                return <Text key={i}>{name}</Text>;
-              })}
-            </Text>
-            <Badge colorScheme="messenger" fontSize="md" mb={3}>
-              {post.drugClass}
-            </Badge>
-            <Flex flexDir="column">
-              <Text mr={2} fontWeight="bold">
-                Brands:
+        {posts.map((post, index) => {
+          return (
+            <GridItem
+              key={index}
+              m={3}
+              p={5}
+              bg={cardBackground}
+              maxH={1000}
+              width={width >= 1000 ? "31%" : "100%"}
+              borderRadius="lg"
+              overflowY="auto"
+              css={{
+                "&::-webkit-scrollbar": {
+                  width: "4px",
+                },
+                "&::-webkit-scrollbar-track": {
+                  width: "3px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#1D4044",
+                  borderRadius: "24px",
+                },
+              }}
+            >
+              <Text fontWeight="bold" fontSize="3xl" mb={3}>
+                {post.drugName.map((name, i) => {
+                  return <Text key={i}>{name}</Text>;
+                })}
               </Text>
-              {post.brandNames.map((brandName, index) => {
-                return (
-                  <List key={index}>
-                    <ListItem my={1} mr={2}>
-                      <ListIcon as={AiFillCheckCircle} color="green.500" />
-                      {brandName}
-                    </ListItem>
-                  </List>
-                );
-              })}
-            </Flex>
-            <Flex>
-              <Text mr={2} fontWeight="bold">
-                Indications:
-              </Text>
-              {post.indication.map((area, index) => {
-                return (
-                  <Text mr={2} key={index}>
-                    {area}
+              <Badge colorScheme="messenger" fontSize="md" mb={3}>
+                {post.drugClass}
+              </Badge>
+              <Flex flexDir="column">
+                <Text mr={2} fontWeight="bold">
+                  Brands:
+                </Text>
+                {post.brandNames.map((brandName, index) => {
+                  return (
+                    <List key={index}>
+                      <ListItem my={1} mr={2}>
+                        <ListIcon as={AiFillCheckCircle} color="green.500" />
+                        {brandName}
+                      </ListItem>
+                    </List>
+                  );
+                })}
+              </Flex>
+              <Flex>
+                <Text mr={2} fontWeight="bold">
+                  Indications:
+                </Text>
+                {post.indication.map((area, index) => {
+                  return (
+                    <Text mr={2} key={index}>
+                      {area}
+                    </Text>
+                  );
+                })}
+              </Flex>
+              <List flexWrap="wrap" mb={2}>
+                <ListItem>
+                  <Text mr={2} fontWeight="bold">
+                    Major Side Effects:
                   </Text>
-                );
-              })}
-            </Flex>
-            <List flexWrap="wrap" mb={2}>
-              <ListItem>
+                  {post.majorSideEffects.map((se, index) => {
+                    return (
+                      <Tag my={1} mr={2} key={index} colorScheme="facebook">
+                        {se}
+                      </Tag>
+                    );
+                  })}
+                </ListItem>
+                <ListItem>
+                  <Text mr={2} fontWeight="bold">
+                    Serious Side Effects:
+                  </Text>
+                  {post.seriousSideEffect.map((se, index) => {
+                    return (
+                      <Tag my={1} mr={2} colorScheme="red" key={index}>
+                        {se}
+                      </Tag>
+                    );
+                  })}
+                </ListItem>
+              </List>
+              <List flexWrap="wrap">
                 <Text mr={2} fontWeight="bold">
-                  Major Side Effects:
+                  Cautions:
                 </Text>
-                {post.majorSideEffects.map((se, index) => {
+                {post.caution.map((warning, index) => {
                   return (
-                    <Tag my={1} mr={2} key={index} colorScheme="facebook">
-                      {se}
-                    </Tag>
+                    <List key={index}>
+                      <ListItem my={1} mr={2}>
+                        <ListIcon as={AiFillWarning} color="green.500" />
+                        {warning}
+                      </ListItem>
+                    </List>
                   );
                 })}
-              </ListItem>
-              <ListItem>
+              </List>
+              <List flexWrap="wrap">
                 <Text mr={2} fontWeight="bold">
-                  Serious Side Effects:
+                  Monitoring Requirement:
                 </Text>
-                {post.seriousSideEffect.map((se, index) => {
+                {post.monitoringRequirement.map((requirement, index) => {
                   return (
-                    <Tag my={1} mr={2} colorScheme="red" key={index}>
-                      {se}
-                    </Tag>
+                    <List key={index}>
+                      <ListItem my={1} mr={2}>
+                        <ListIcon as={AiFillCheckCircle} color="green.500" />
+                        {requirement}
+                      </ListItem>
+                    </List>
                   );
                 })}
-              </ListItem>
-            </List>
-            <List flexWrap="wrap">
-              <Text mr={2} fontWeight="bold">
-                Cautions:
-              </Text>
-              {post.caution.map((warning, index) => {
-                return (
-                  <List key={index}>
-                    <ListItem my={1} mr={2}>
-                      <ListIcon as={AiFillWarning} color="green.500" />
-                      {warning}
-                    </ListItem>
-                  </List>
-                );
-              })}
-            </List>
-            <List flexWrap="wrap">
-              <Text mr={2} fontWeight="bold">
-                Monitoring Requirement:
-              </Text>
-              {post.monitoringRequirement.map((requirement, index) => {
-                return (
-                  <List key={index}>
-                    <ListItem my={1} mr={2}>
-                      <ListIcon as={AiFillCheckCircle} color="green.500" />
-                      {requirement}
-                    </ListItem>
-                  </List>
-                );
-              })}
-            </List>
-            <List flexWrap="wrap">
-              <ListItem mr={2} fontWeight="bold">
-                Remarks:
-              </ListItem>
-              {post.remarks.map((remark, index) => {
-                return (
-                  <List key={index}>
-                    <ListItem my={1} mr={2}>
-                      <ListIcon as={AiFillCheckCircle} color="green.500" />
-                      {remark}
-                    </ListItem>
-                  </List>
-                );
-              })}
-            </List>
-            <Accordion mt={5} allowToggle>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      <Text fontWeight="bold">Mechanism of Actions</Text>
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>{post.mechanismOfAction}</AccordionPanel>
-              </AccordionItem>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      <Text fontWeight="bold">Reference</Text>
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  <Button>
-                    <a
-                      href="https://www.medicines.org.uk/emc/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      SPC
-                    </a>
-                  </Button>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </GridItem>
-        );
-      })}
+              </List>
+              <List flexWrap="wrap">
+                <ListItem mr={2} fontWeight="bold">
+                  Remarks:
+                </ListItem>
+                {post.remarks.map((remark, index) => {
+                  return (
+                    <List key={index}>
+                      <ListItem my={1} mr={2}>
+                        <ListIcon as={AiFillCheckCircle} color="green.500" />
+                        {remark}
+                      </ListItem>
+                    </List>
+                  );
+                })}
+              </List>
+              <Accordion mt={5} allowToggle>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        <Text fontWeight="bold">Mechanism of Actions</Text>
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    {post.mechanismOfAction}
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        <Text fontWeight="bold">Reference</Text>
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <Button>
+                      <a
+                        href="https://www.medicines.org.uk/emc/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        SPC
+                      </a>
+                    </Button>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </GridItem>
+          );
+        })}
       </Flex>
     </Flex>
   );
