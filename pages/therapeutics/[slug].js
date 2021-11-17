7705ea052324d1a24cbd8be732b1529a5e13186f
John Doe <84591288+ocwilsonchow@users.noreply.github.com>
2021-11-17 17:12:40 +0800

    update

--- pages/therapeutics/[slug].js
+++ pages/therapeutics/[slug].js
@@ -1,15 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import {Button} from "@chakra-ui/react"
+import { Button, Center, Flex, Text } from "@chakra-ui/react";
 
 function dynamic() {
   return (
-    <div>
-      <h1>dynamic slug page</h1>
-      <Link href="/">
-        <Button>Home</Button>
-      </Link>
-    </div>
+    <>
+      <Flex justifyContent="center" alignItems='center' flexDir="column" p={10}>
+        <h1>dynamic slug page</h1>
+        <Center my={10} p={5} bg="black" fontWeight="bold">
+            Nice! The dynamic routing is working! 👊🏻
+        </Center>
+        <Link href="/">
+          <Button my={10}>Go back to home page </Button>
+        </Link>
+      </Flex>
+    </>
   );
 }
 
