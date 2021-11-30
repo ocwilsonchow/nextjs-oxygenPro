99a8e96108df7e653496c64839cfef64e183616b
John Doe <84591288+ocwilsonchow@users.noreply.github.com>
2021-11-30 16:23:00 +0800

    update

--- pages/signIn.js
+++ pages/signIn.js
@@ -12,7 +12,7 @@ import {
   Input,
   Button,
   Divider,
-  Center
+  Center,
 } from "@chakra-ui/react";
 import { getPosts, getTopics } from "../services";
 
@@ -22,12 +22,24 @@ export default function SignIn({ topics }) {
   return (
     <>
       <Header topics={topics} />
-      <Flex flexDir="column" justifyContent="center" alignItems="center" py={10}>
-        <Text fontWeight="bold" fontSize="4xl">
+      <Flex
+        flexDir="column"
+        justifyContent="center"
+        alignItems="center"
+        p={5}
+      >
+        <Text fontWeight="bold" fontSize="4xl" align="center">
           Create your account
         </Text>
-        <Center m={5}>Development in progress 🔧</Center>
-        <Flex p={10} bg={cardColor} w="400px" borderRadius="1rem" flexDir="column" boxShadow="lg">
+        <Center  align="center" m={5}>Development in progress 🔧</Center>
+        <Flex
+          p={10}
+          bg={cardColor}
+          w={["100%","80%","400px","400px"]}
+          borderRadius="1rem"
+          flexDir="column"
+          boxShadow="xl"
+        >
           <FormControl id="email" mb={5}>
             <FormLabel>Email address</FormLabel>
             <Input type="email" />
@@ -35,18 +47,15 @@ export default function SignIn({ topics }) {
           <FormControl id="password" mb={5}>
             <FormLabel>Password</FormLabel>
             <Input type="password" />
-            
           </FormControl>
           <FormControl id="confirm-password" mb={5}>
             <FormLabel>Confirm Password</FormLabel>
             <Input type="password" />
-            
           </FormControl>
           <Button mb={5}>Create account</Button>
           <Divider />
           <Box mt={5}>Already have an account? Log in to Oxygen</Box>
         </Flex>
-       
       </Flex>
     </>
   );
